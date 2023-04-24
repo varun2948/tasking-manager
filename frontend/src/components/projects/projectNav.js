427fe77335a482e05f0bddf3bbc0d52349ee1473
Taylor Smock <tsmock@meta.com>
2023-04-24 08:21:20 -0600

    Improve treeshaking
    * Avoid default imports in ReactJS
      There were quite a few files where we imported react, but didn't
      actually use it.
    * Use non-default imports where possible
    * Use named imports where possible
    
    Signed-off-by: Taylor Smock <tsmock@meta.com>

--- frontend/src/components/projects/projectNav.js
+++ frontend/src/components/projects/projectNav.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import { useSelector, useDispatch } from 'react-redux';
